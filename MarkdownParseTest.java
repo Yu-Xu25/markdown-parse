5ef83390134eb0cf1698b8d67ac01b0cb3538afd
Yu-Xu25 <yux032@ucsd.edu>
2022-02-03 12:48:00 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,3 +1,6 @@
+//javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
+//java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
+
 import static org.junit.Assert.assertEquals;
 
 import java.io.ByteArrayOutputStream;
@@ -12,47 +15,47 @@ import static org.junit.Assert.*;
 public class MarkdownParseTest {
     @Test
     public void testEmpty() throws IOException {
-        assertLinks(List.of(), "testCases/empty.md");
+        assertLinks(List.of(), "/Users/yuxu/Documents/GitHub/markdown-parse/testCases/empty.md");
     }
 
     @Test
     public void testExtraSpace() throws IOException {
-        assertLinks(List.of(), "testCases/extraSpace.md");
+        assertLinks(List.of(), "/Users/yuxu/Documents/GitHub/markdown-parse/testCases/extraSpace.md");
     }
 
     @Test
     public void testEscape() throws IOException {
-        assertLinks(List.of("https://somethingelse.com"), "testCases/escape.md");
+        assertLinks(List.of("https://somethingelse.com"), "/Users/yuxu/Documents/GitHub/markdown-parse/testCases/escape.md");
     }
 
     @Test
     public void testJustEscape() throws IOException {
-        assertLinks(List.of(), "testCases/justEscape.md");
+        assertLinks(List.of(), "/Users/yuxu/Documents/GitHub/markdown-parse/testCases/justEscape.md");
     }
 
     @Test
     public void testImage() throws IOException {
-        assertLinks(List.of(), "testCases/image.md");
+        assertLinks(List.of(), "/Users/yuxu/Documents/GitHub/markdown-parse/testCases/image.md");
     }
 
     @Test
     public void testJustBrackets() throws IOException {
-        assertLinks(List.of(), "testCases/justBrackets.md");
+        assertLinks(List.of(), "/Users/yuxu/Documents/GitHub/markdown-parse/testCases/justBrackets.md");
     }
 
     @Test
     public void testJustParentheses() throws IOException {
-        assertLinks(List.of(), "testCases/justParentheses.md");
+        assertLinks(List.of(), "/Users/yuxu/Documents/GitHub/markdown-parse/testCases/justParentheses.md");
     }
 
     @Test
     public void testMultiline() throws IOException {
-        assertLinks(List.of("https://isthisfound.com"), "testCases/multiline.md");
+        assertLinks(List.of("https://isthisfound.com"), "/Users/yuxu/Documents/GitHub/markdown-parse/testCases/multiline.md");
     }
 
     @Test
     public void testLastLine() throws IOException {
-        assertLinks(List.of("last line link should be found"), "testCases/lastLine.md");
+        assertLinks(List.of("last line link should be found"), "/Users/yuxu/Documents/GitHub/markdown-parse/testCases/lastLine.md");
     }
 
     public static void assertLinks(List<String> expectedLinks, String fileName) throws IOException {
